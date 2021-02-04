d1ef8b69a529676999e2334628a1033e9289441a
slariath <yunusck00@hotmail.com>
2021-02-04 21:27:13 +0300

    LAB-5 QRD1114 INIT

---
+++ Lab-5 QRD1114/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+#define qrdPin A0
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("READY");
+  pinMode(qrdPin, INPUT);
+}
+
+void loop()
+{
+  Serial.println(analogRead(qrdPin));
+}
