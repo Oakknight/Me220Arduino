85fb1464b9c89038250a7ca55efc79f9dd90a977
slariath <yunusck00@hotmail.com>
2021-02-07 00:18:02 +0300

    Double DC Motor w/ Speed Control

--- Term Project/src/main.cpp
+++ Term Project/src/main.cpp
@@ -4,34 +4,34 @@
 #define blueBtn 3
 #define analogPin A5
 
+//Right Engine Pins
 bool digital8 = false;
 bool digital9 = false;
 bool digital10 = false;
+const int rightEnable = 11;
 
+//Left Engine Pins
 bool digital2 = false;
 bool digital3 = true;
+const int leftEnable = 11;
 
-int val = 0;
+//Right Sensor Pins
+const int rightTrigger = 3;
+const int rightEcho = 4;
 
-void NotifyButtonWhite()
-{
-  Serial.println("You have pressed the white button (pin 2) ");
-  //digitalWrite()
-}
+//Left Sensor Pins
+const int leftTrigger = 6;
+const int leftEcho = 7;
 
-void NotifyButtonBlue()
-{
-  Serial.println("You have pressed the blue button (pin 3) ");
-  //digital
-}
+int speedValue = 0;
 
 void setup()
 {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(2), NotifyButtonWhite, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), NotifyButtonBlue, RISING);
+  // attachInterrupt(digitalPinToInterrupt(2), NotifyButtonWhite, RISING);
+  // attachInterrupt(digitalPinToInterrupt(3), NotifyButtonBlue, RISING);
 
   pinMode(whiteBtn, INPUT);
   pinMode(blueBtn, INPUT);
@@ -39,23 +39,45 @@ void setup()
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  digitalWrite(9, HIGH);
+  digitalWrite(8, LOW);
+
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
 }
 
 void loop()
 {
-  val = analogRead(analogPin);
-  Serial.println(val);
+
+  while (Serial.available() == 0)
+  {
+  }
+
+  speedValue = Serial.parseInt(); //va
+
+  analogWrite(10, speedValue);
+  analogWrite(11, speedValue);
+
+  Serial.println(speedValue);
+
+  /*/
+  speedValue = analogRead(analogPin);
+  Serial.println(speedValue);
 
   if (digitalRead(whiteBtn))
   {
-    analogWrite(10, val/4);
+    analogWrite(10, speedValue/4);
   }
   else
   {
     digitalWrite(10, LOW);
   }
 
-  // Value of the digital 10 pin is always same with digital 2
+  // speedValueue of the digital 10 pin is always same with digital 2
   if (!digitalRead(blueBtn))
     digitalWrite(8, HIGH);
   else
@@ -64,4 +86,5 @@ void loop()
     digitalWrite(9, HIGH);
   else
     digitalWrite(9, LOW);
+    //*/
 }
