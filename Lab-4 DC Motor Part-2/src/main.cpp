ceedab0008811ff8719ce00b9380ff3f68a1b80c
slariath <yunusck00@hotmail.com>
2021-02-11 03:37:45 +0300

    Wrote some code for the communication protocol

--- Lab-4 DC Motor Part-2/src/main.cpp
+++ Lab-4 DC Motor Part-2/src/main.cpp
@@ -1,67 +1,198 @@
 #include <Arduino.h>
 
-#define whiteBtn 2
-#define blueBtn 3
-#define analogPin A5
+/*/WARNING
+I have salvaged some of the code and electronics for this lab from my project
 
-bool digital8 = false;
-bool digital9 = false;
-bool digital10 = false;
+The Functions we need to provide:
+- Apply a certain DC value to a specified motor
+- Apply different DC values to each motor
+- Stop an individual motor
+- Stop all motors
+- Change direction of a motor
+- Change direction of both motors
 
-bool digital2 = false;
-bool digital3 = true;
+So basically, we have three main actions: Stop, Change direction, and apply DC value.
 
-int val = 0;
+Also, we have three identifiers for each action: Led Motor, DC Motor or All Motors.And an extra duty cycle value if we want
+to change it. 
+/
+The first char will specify our action and will be stored in the actionChar variable:
+'A': To apply a certain DC Value,
+'C': To change the direction,
+'S': To stop the motor which will be specified in the next char
+The second charachter will specify our target:
+'A': For all motors,
+'L': For the LED Motor,
+'D': For the DC Motor.
 
-void NotifyButtonWhite()
-{
-  Serial.println("You have pressed the white button (pin 2) ");
-  //digitalWrite()
+And if the user provides an integer value at the end, that will be our dc value.
+
+Therefore, we need three functions for the actions, and three variables to process the input
+
+Example Inputs:
+AA500, will apply a pwm value of 500 to all motors
+
+SL, will stop the LED Motor
+
+CD, will change the direction of the DC Motor
+
+and so on...
+
+//*/
+
+//DC Motor Pins
+const int dcEnable = 10; // How fast will our motor turn? Not to be mistaken with duty cycle
+const int dcA = 9;
+const int dcB = 8;
+byte dcDirection = 0;
+
+//Led Motor Pins
+const int ledEnable = 11; //How bright will our LED be
+const int ledA = 12;
+const int ledB = 13;
+byte ledDirection = 0; // This variable will keep the direction of turning, 1 for when A is high and 2 for when B is HIGH
+
+//The variables we will use to process the input
+String protocolInput = ""; //This will store the input from the user
+//char actionChar;           // This will store our first charachter.
+char targetChar;     // This will store the target of our actions
+String dcValue = ""; // This will store our dc value if we have one
+
+void changeDirection()
+{ //This function will change the direction of our motors
+
+  if (targetChar == 'L')
+  {
+    //For the LED Motor
+    if (ledDirection == 1)
+    {
+      digitalWrite(ledA, LOW);
+      digitalWrite(ledB, HIGH);
+    }
+    else if (ledDirection == 2)
+    {
+      digitalWrite(ledA, HIGH);
+      digitalWrite(ledB, LOW);
+    }
+  }
+  else if (targetChar == 'D')
+  {
+    //For DC Motor
+    if (dcDirection == 1)
+    {
+      digitalWrite(dcA, LOW);
+      digitalWrite(dcB, HIGH);
+    }
+    else if (dcDirection == 2)
+    {
+      digitalWrite(dcA, HIGH);
+      digitalWrite(dcB, LOW);
+    }
+  }
+  else if (targetChar == 'A')
+  { //For all motors
+    if (dcDirection == 1)
+    {
+      digitalWrite(dcA, LOW);
+      digitalWrite(dcB, HIGH);
+    }
+    else if (dcDirection == 2)
+    {
+      digitalWrite(dcA, HIGH);
+      digitalWrite(dcB, LOW);
+    }
+    if (ledDirection == 1)
+    {
+      digitalWrite(ledA, LOW);
+      digitalWrite(ledB, HIGH);
+    }
+    else if (ledDirection == 2)
+    {
+      digitalWrite(ledA, HIGH);
+      digitalWrite(ledB, LOW);
+    }
+  }
 }
 
-void NotifyButtonBlue()
+void stopMotor()
 {
-  Serial.println("You have pressed the blue button (pin 3) ");
-  //digital
+  //This will stop the specified motor(s)
+  if (targetChar == 'A')
+  {
+    digitalWrite(ledEnable, LOW);
+    digitalWrite(dcEnable, LOW);
+  }
+  else if (targetChar == 'L')
+  {
+    //Stop Led Motor
+    digitalWrite(ledEnable, LOW);
+  }
+  else if (targetChar == 'D')
+  {
+    //Stop DC Motor
+    digitalWrite(dcEnable, LOW);
+  }
+}
+
+void applydc()
+{
+  if (targetChar == 'A')
+  {
+    analogWrite(ledEnable, dcValue.toInt());
+    analogWrite(dcEnable, dcValue.toInt());
+  }
+  else if (targetChar == 'L')
+  {
+    analogWrite(ledEnable, dcValue.toInt());
+  }
+  else if (targetChar == 'D')
+  {
+    analogWrite(dcEnable, dcValue.toInt());
+  }
 }
 
 void setup()
 {
   // put your setup code here, to run once:
-
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(2), NotifyButtonWhite, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), NotifyButtonBlue, RISING);
 
-  pinMode(whiteBtn, INPUT);
-  pinMode(blueBtn, INPUT);
-
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+  for (int i = 8; i < 14; i++) // Set the pins with a loop
+  {
+    pinMode(i, OUTPUT);
+  }
 }
 
 void loop()
 {
-  val = analogRead(analogPin);
-  Serial.println(val);
-
-  if (digitalRead(whiteBtn))
+  Serial.println("Please enter data");
+  while (Serial.available() == 0)
   {
-    analogWrite(10, val/4);
-  }
-  else
-  {
-    digitalWrite(10, LOW);
+    // Do nothing till input
   }
 
-  // Value of the digital 10 pin is always same with digital 2
-  if (!digitalRead(blueBtn))
-    digitalWrite(8, HIGH);
-  else
-    digitalWrite(8, LOW);
-  if (digitalRead(blueBtn) && digitalRead(whiteBtn))
-    digitalWrite(9, HIGH);
-  else
-    digitalWrite(9, LOW);
+  protocolInput = Serial.readString();  // Read Input
+  targetChar = protocolInput.charAt(1); // Store our target
+  Serial.print("Our target is: ");
+  Serial.println(targetChar);
+
+  if (protocolInput.charAt(0) == 'C')
+  {
+    changeDirection();
+  }
+  if (protocolInput.charAt(0) == 'S')
+  {
+    //Stop the motor specified
+    stopMotor();
+  }
+  if (protocolInput.charAt(0) == 'A')
+  {
+    //Parse the integer value for dc
+    dcValue = "";
+    for (size_t i = 2; i < protocolInput.length(); i++)
+    { // This for loop will get us the integer value at the end
+      dcValue += protocolInput.charAt(i);
+      Serial.println(dcValue);
+    }
+    applydc();
+  }
 }
